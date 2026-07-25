d61e9fff6bc18b1ca62a13e1d3265a5ff8277076
Muhammad Hamza <hamzanoor79@gmail.com>
2026-07-25 10:10:06 +0500

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const app = express();
 
 const PORT = 3000;
 
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!');
+// });
+
+// GET /
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.json({
+        name: "Task API",
+        version: "1.0",
+        endpoints: ["/tasks"]
+    });
 });
+
+// GET /health
+app.get('/health', (req, res) => {
+    res.json({
+        status: "ok"
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
